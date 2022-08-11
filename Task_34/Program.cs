9cb9667aeebcc665b5cfbaca0fe7ed3669ec7b83
ViktorSivashev <viktor.sivashev@mail.ru>
2022-08-11 21:06:11 +0300

    task_home

--- Task_34/Program.cs
+++ Task_34/Program.cs
@@ -2,35 +2,20 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-int[] numbers = new int[4];
+int[] array = new int[4];
+int countEven = 0;
 
-void Array1(int[] array, int min, int max){
-  for (int i = 0; i<array.Length; i++ ){
-    array[i] = new Random().Next(min, max);
-  }
-}
-
-void Array2(int[] array){
-    for (int i = 0; i<array.Length; i++ ){
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100, 999);
     Console.Write(array[i] + " ");
-  }
-  Console.WriteLine();
 }
-
-int even1(int[] array){
-    int even = 0;
-    for (int i = 0; i<array.Length; i++ ){
-    if (array[i] % 2 == 0)
-    {
-      even++;
-    }
-  }
-  return even;
-}
-
-Array1(numbers, 100, 1000);
-Array2(numbers);
 Console.WriteLine();
 
-int even = even1(numbers);
-Console.WriteLine($"Количество чётных чисел в массиве: {even}");
+for (int j = 0; j < array.Length; j++)
+{
+    if (array[j] % 2 == 0)
+    countEven = countEven + 1;
+     
+}
+Console.Write("Четные числа из массива: " + countEven);
--- Task_35/Program.cs
+++ Task_35/Program.cs
@@ -6,3 +6,19 @@
 // [10, 11, 12, 13, 14] -> 5
 
 
+int[] array = new int[123];
+int count=0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(0, 1000);
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+for (int j = 0; j < array.Length; j++)
+{
+    if(array[j] > 9 & array[j] < 100) count = count + 1;
+}
+Console.WriteLine();
+Console.Write("В массиве следующее кол-во элементов на отрезке [10,99] - ");
+Console.Write(count);
